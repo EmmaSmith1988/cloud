b19cc41073f1df0940d157c75076b2557c4efc5c
Emma Smith <smileandsparkle@hotmail.com>
2022-05-30 11:37:30 +0100

    initial commit

---
+++ src/main/java/com/example/cloudbusting/CloudBustingApplication.java
@@ -0,0 +1,16 @@
+package com.example.cloudbusting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class CloudBustingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudBustingApplication.class, args);
+	}
+
+	public static class Controller {
+	}
+}
